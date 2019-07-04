908b1a0068767321a264f1a3913021e83af6523f
02ias01 <02ias01@zrt.aa.local>
2019-07-04 09:31:02 +0300

    -Barcoder added

---
+++ AP.Barcoder/Base1DCodeIntCS.cs
@@ -0,0 +1,15 @@
+﻿using Barcoder.Utils;
+
+namespace Barcoder
+{
+    public class Base1DCodeIntCS : Base1DCode, IBarcodeIntCS
+    {
+        internal Base1DCodeIntCS(BitList bitList, string kind, string content, int checksum)
+            : base(bitList, kind, content)
+        {
+            Checksum = checksum;
+        }
+
+        public int Checksum { get; }
+    }
+}
