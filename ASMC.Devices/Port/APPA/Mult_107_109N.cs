2e81ae95e83e012e6ec92bfd014a3a484b2cf1c1
02zaa01 <02zaa01@02-029-003>
2020-07-10 12:46:19 +0300

    Добавлен файл интрефейса для APPA-107N-109N

---
+++ ASMC.Devices/Port/APPA/Mult_107_109N.cs
@@ -0,0 +1,228 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ASMC.Devices.Port.APPA
+{
+    public class Mult107109N : ComPort
+    {
+        private readonly System.Timers.Timer _wait;
+        private bool _flagTimeout;
+        private List<byte> _data;
+        private const byte Cadr = 19;
+        private readonly List<byte> _byffer;
+        private readonly byte[] _sendData = { 0x55, 0x55, 0x00, 0x00, 0xAA };
+        static readonly AutoResetEvent WaitEvent = new AutoResetEvent(false);
+        public double Value
+        {
+            get
+            {
+                SendQuery();
+                double value;
+                WaitEvent.WaitOne();
+                if (_flagTimeout)
+                {
+                    _flagTimeout = false;
+                    throw new TimeoutException();
+                }
+                switch (_data[11] & 0x07)
+                {
+                    case (int)Point.None:
+                        value = (_data[10] << 8) | (_data[9] << 8) | (_data[8]);
+                        break;
+                    case (int)Point.Point1:
+                        value = ((_data[10] << 8) | (_data[9] << 8) | (_data[8])) / 10.0;
+                        break;
+                    case (int)Point.Point2:
+                        value = ((_data[10] << 8) | (_data[9] << 8) | (_data[8])) / 100.0;
+                        break;
+                    case (int)Point.Point3:
+                        value = ((_data[10] << 8) | (_data[9] << 8) | (_data[8])) / 1000.0;
+                        break;
+                    case (int)Point.Point4:
+                        value = ((_data[10] << 8) | (_data[9] << 8) | (_data[8])) / 10000.0;
+                        break;
+                    default:
+                        return 0;
+                }
+                return value; 
+            }
+        }
+        public Mult107109N(string portName) : base(portName)
+        {
+            _wait = new System.Timers.Timer();
+            _byffer = new List<byte>();
+            _data = new List<byte>();
+            _flagTimeout = false;
+            _wait.Interval = 35000;
+            _wait.Elapsed += TWait_Elapsed;
+            _wait.AutoReset = false;
+        }
+        private void TWait_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            _flagTimeout = true;
+            WaitEvent.Set();
+        }
+        public void SendQuery()
+        {
+            if (Sp.IsOpen)
+            {
+                Sp.DataReceived += SerialPort_DataReceived;
+                Sp.Write(_sendData, 0, _sendData.Length);
+                _wait.Start();
+            }
+            else
+            {
+                Close();
+            }
+
+        }
+        /// <summary>
+        /// Положение переключатиля
+        /// </summary>
+        public enum Rotor
+        {
+            V = 0x01,
+            MV = 0x02,
+            Ohm = 0x03,
+            Diode = 0x04,
+            MA = 0x05,
+            A = 0x06,
+            Cap = 0x07,
+            Hz = 0x08,
+            Temp = 0x09
+        }
+        public enum Point
+        {
+            None = 0,
+            Point1 = 0x01,
+            Point2 = 0x02,
+            Point3 = 0x03,
+            Point4 = 0x04
+        }
+        public enum Range
+        {
+            Range1Manual = 0x80,
+            Range2Manual = 0x81,
+            Range3Manual = 0x82,
+            Range4Manual = 0x83,
+            Range5Manual = 0x84,
+            Range6Manual = 0x85,
+            Range7Manual = 0x86,
+            Range8Manual = 0x87,
+            Range1Auto = 0,
+            Range2Auto = 0x01,
+            Range3Auto = 0x02,
+            Range4Auto = 0x03,
+            Range5Auto = 0x04,
+            Range6Auto = 0x05,
+            Range7Auto = 0x06,
+            Range8Auto = 0x07
+
+        }
+        public enum BlueState
+        {
+            NoPress = 0,
+            OnPress = 0x01,
+            DoublePress = 0x02
+        }
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            _byffer.Clear();
+            var port = (SerialPort)sender;
+            try
+            {
+                byte bt = (byte)port.ReadByte();
+                _byffer.Add(bt);
+                while (_byffer.Count < Cadr)
+                {
+                    if (0x55 == _byffer[0])
+                    {
+                        _byffer.Add((byte)port.ReadByte());
+                    }
+                }
+                Sp.DiscardInBuffer();
+                ChechetSumm();
+                Sp.DataReceived -= SerialPort_DataReceived;
+            }
+            catch (Exception a)
+            {
+                Console.WriteLine(a.ToString());
+            }
+
+        }
+        bool ChechetSumm()
+        {
+            if (_byffer.Count == Cadr)
+            {
+                var chechSum = 0;
+                for (var i = 0; i < _byffer.Count - 1; i++)
+                {
+                    chechSum = chechSum + _byffer[i];
+                }
+                if (_byffer[_byffer.Count - 1] != (chechSum & 0xFF))
+                {
+                    SendQuery();
+                    WaitEvent.Reset();
+                    return false;
+                }
+
+                _data = _byffer;
+                _wait.Stop();
+                WaitEvent.Set();
+
+                return true;
+            }
+
+            return false;
+        }
+        
+
+        public Range GetRange
+        {
+            get
+            {
+                SendQuery();
+                WaitEvent.WaitOne();
+                if (_flagTimeout)
+                {
+                    _flagTimeout = false;
+                    throw new TimeoutException();
+                }
+                return (Range)_data[7];
+            }
+        }
+        public Rotor GetRotor
+        {
+            get
+            {
+                SendQuery();
+                WaitEvent.WaitOne();
+                if (_flagTimeout)
+                {
+                    _flagTimeout = false;
+                    throw new TimeoutException();
+                }
+                return (Rotor)_data[4];
+            }
+        }
+        public BlueState GetBlueState
+        {
+            get
+            {
+                SendQuery();
+                WaitEvent.WaitOne();
+                if (_flagTimeout)
+                {
+                    _flagTimeout = false;
+                    throw new TimeoutException();
+                }
+                return (BlueState)_data[5];
+            }
+        }
+    }
+}
---
+++ Plugins/APPA/APPA-107N-109N/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPA_107N_109N
+{
+    public class Class1
+    {
+    }
+}
