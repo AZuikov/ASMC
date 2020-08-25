2f3b6d58862d9a3a226ecb41276d7cd1f8048449
02tav01 <02tav01@zrt.aa.local>
2020-08-25 15:42:17 +0300

    1

--- Plugins/Test/Test.cs
+++ Plugins/Test/Test.cs
@@ -5,200 +5,106 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using ASMC.Core.Model;
 using ASMC.Data.Model;
 using ASMC.Data.Model.Interface;
 
 namespace Plugins.Test
 {
  
-    public class Device  : IProgram
+    public class Device  : Program
     {
-        public string Type { get; set; }
-        public string Grsi { get; set; }
-
-        public string Range{ get; set; }
-
-        public string Accuracy
+        /// <inheritdoc />
+        public Device(ServicePack service) : base(service)
         {
-            get; set;
+            this.Type = "Тест";
+            Operation = new Verefication(service);
         }
-
-        public AbstraktOperation AbstraktOperation { get; }
-
-
-        public Device()
-        {
-            AbstraktOperation = new Operation();
-            Type = "34401";
-            Grsi = "СССР";
-            Range = "1-5А";
-            Accuracy = "0-5";
-        }
-
     }
 
-    public class DeviceInterface : IDevice
+    public class Verefication : OperationMetrControlBase
     {
-        public string Description
-        {
-            get; set;
-        }
-        public string[] Name
-        {
-            get; set;
-        }
-        public string SelectedName
-        {
-            get; set;
-        }
-        public string StringConnect
-        {
-            get; set;
-        }
-        public void Setting()
-        {
-            throw new NotImplementedException();
-        }
 
-        public bool? IsConnect
+        public Verefication(ServicePack servicePac)
         {
-            get;
+            this.UserItemOperationPrimaryVerf = new OpertionFirsVerf(servicePac);
         }
     }
-    public class Operation : AbstraktOperation
+
+    public class OpertionFirsVerf : Operation
     {
-        public Operation()
+        /// <inheritdoc />
+        public OpertionFirsVerf(ServicePack servicePack) : base(servicePack)
         {
-            this.UserItemOperationPrimaryVerf = new OpertionFirsVerf() ;
-            
-        } 
-    }
-
-    public class OpertionFirsVerf : IUserItemOperation
-    {
-        public void RefreshDevice()
-        {
-            AddresDivece = new[] { "COM5", "COM6" }; 
-        }
-
-        public IDevice[] Device { get; }
-        public IUserItemOperationBase[] UserItemOperation { get; }
-        public string[] Accessories { get; }
-        public string[] AddresDivece { get; set; }
-
-        public OpertionFirsVerf()
-        {
-            Device = new[]
+            ControlDevices = new IDeviceUi[]
             {
-                new DeviceInterface{ Name = new []{"344010A"}, StringConnect = "COM1", Description = "Мультиметр"},
-                new DeviceInterface{ Name = new []{"APPA-106", "APPA-107"}, StringConnect = "COM12" } ,
-                new DeviceInterface{ Name = new []{"APP32A-106", "APPA-107"}, StringConnect = "COM12" },
-                new DeviceInterface{ Name = new []{"AP54545PA-106", "APPA-107"}, StringConnect = "COM12" }
+                new DeviceInterface {Name = new[] {"34401A"}, Description = "Мультиметр"},
+                new DeviceInterface {Name = new[] {"В3"}, Description = "Микровольтметр", IsCanStringConnect = false}
             };
-            Accessories = new[] {"Мультиметр 344010A", "Набор проводов"};
-      
-            var opertio3 = new ItemOperation2 {Name = "Измерение напряжения"};
-            opertio3.Nodes.Add(new ItemOperation3 {Name = "Измерение постоянного тока"});   
-            UserItemOperation = new IUserItemOperationBase[] { new ItemOperation1 { Name = "Опробывание" }, opertio3 };
-        }
-    }
-    public class ItemOperation1  : AbstractUserItemOperationBase, IUserItemOperation<string>
-    {
-        public ItemOperation1()
-        {
-            Sheme = new ShemeImage() {Number = 1, Path = ""};
-        }
-
-        public override void StartSinglWork(Guid guid)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<IBasicOperation<string>> DataRow { get; set; }
-
-        public override void StartWork()
-        {
-            Thread.Sleep(30000);
-            //throw new NotImplementedException();
-        }
-
-        protected override DataTable FillData()
-        {
-            var data = new DataTable();
-            data.Columns.Add("Измеренное значение");
-            data.Columns.Add("Ожидаемое значение");
-            foreach(var row in DataRow)
+            this.UserItemOperation = new IUserItemOperationBase[] {new Operation1(this)};
+                Accessories = new[]
             {
-                var dataRow = data.NewRow();
-                var dds = row as BasicOperationVerefication<double>;
-                dataRow[0] = dds.Getting;
-                dataRow[1] = dds.Expected;
-                data.Rows.Add(dataRow);
-            }
-
-            return data;
-        }
-    }
-    public class ItemOperation2 : AbstractUserItemOperationBase, IUserItemOperation<double?>
-    {
-        public override void StartSinglWork(Guid guid)
-        {
-            throw new NotImplementedException();
-        } 
-        
-        public List<IBasicOperation<double?>> DataRow { get; set; }
-    
-       
-        public override void StartWork()
-        {
-            throw new NotImplementedException();
-        }
-
-        public ItemOperation2()
-        {
-            DataRow = new List<IBasicOperation<double?>>
-            {
-                new BasicOperationVerefication<double?>{Getting = 512.3, Expected = 512.0},
-                new BasicOperationVerefication<double?> {Getting = 5.001, Expected = 5.005},  
-                new BasicOperationVerefication<double?> {Getting = null , Expected = 5.005},
+                "Мультиметр цифровой Agilent/Keysight 34401A",
+                "Кабель banana"
             };
         }
-        protected override DataTable FillData()
+
+        /// <inheritdoc />
+        public override void RefreshDevice()
         {
-            var data = new DataTable();
-            data.Columns.Add("Измеренное значение");
-            data.Columns.Add("Ожидаемое значение");
-            foreach(var row in DataRow)
-            {
-                var dataRow = data.NewRow();
-                var dds = row as BasicOperationVerefication<double?>;
-                dataRow[0] = dds.Getting;
-                dataRow[1] = dds.Expected;
-                data.Rows.Add(dataRow);
-            }
-
-            return data;
+           return;
         }
-       
-    }
 
-    public class ItemOperation3 : AbstractUserItemOperationBase, IUserItemOperation<double?>
+        /// <inheritdoc />
+        public override void FindDivice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class DeviceInterface :  IDeviceUi
     {
-        public override void StartSinglWork(Guid guid)
+        /// <inheritdoc />
+        public string Description { get; set; }
+
+        /// <inheritdoc />
+        public bool IsCanStringConnect { get; set; }
+
+        /// <inheritdoc />
+        public bool? IsConnect { get; }
+
+        /// <inheritdoc />
+        public string[] Name { get; set; }
+
+        /// <inheritdoc />
+        public string SelectedName { get; set; }
+
+        /// <inheritdoc />
+        public string StringConnect { get; set; }
+    }
+    public class Operation1 : ParagraphBase, IUserItemOperation<double>
+    {
+        /// <inheritdoc />
+        public Operation1(IUserItemOperation userItemOperation) : base(userItemOperation)
         {
-            throw new NotImplementedException();
+            Name = "Внешний осмотр";
+            DataRow = new List<IBasicOperation<double>>();
         }
 
-        public override void StartWork()
-        {
-            throw new NotImplementedException();
-        }
 
+        /// <inheritdoc />
         protected override DataTable FillData()
         {
-            throw new NotImplementedException();
+            return null;
         }
 
-        public List<IBasicOperation<double?>> DataRow { get; set; }
+        /// <inheritdoc />
+        protected override void InitWork()
+        {
+         return;
+        }
+
+        /// <inheritdoc />
+        public List<IBasicOperation<double>> DataRow { get; set; }
     }
+
 }
