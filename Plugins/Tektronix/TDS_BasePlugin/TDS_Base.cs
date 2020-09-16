a602ed8aaea44828cf66bd95d393be2ea15aea47
02zaa01 <02zaa01@02-029-003>
2020-09-16 09:46:23 +0300

    1

--- Plugins/Tektronix/TDS_BasePlugin/TDS_Base.cs
+++ Plugins/Tektronix/TDS_BasePlugin/TDS_Base.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ASMC.Core.Model;
 
 namespace TDS_BasePlugin
 {
-    public class TDS_Base
+    public class TDS_Base:Program
+
     {
+        public TDS_Base(ServicePack service) : base(service)
+        {
+            Grsi = "32618-06";
+        }
+    }
+
+    public class Operation : OperationMetrControlBase
+    {
+        public Operation()
+        {
+            UserItemOperationPeriodicVerf = UserItemOperationPrimaryVerf;
+        }
+    }
+
+    public abstract class OpertionFirsVerf : ASMC.Core.Model.Operation
+    {
+        public OpertionFirsVerf(ServicePack servicePack) : base(servicePack)
+        {
+            
+        }
     }
 }
