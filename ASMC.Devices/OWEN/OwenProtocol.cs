a9df86e3fb4f614c03d1b215656a7bcd96e254a4
02zaa01 <02zaa01@02-029-003>
2020-10-08 09:14:04 +0300

    Добавлены обертки для сокращения последующего кода.
    Упрощенное считвание параметров в зависимости от типа возвращаемых прибором данных

--- ASMC.Devices/OWEN/OwenProtocol.cs
+++ ASMC.Devices/OWEN/OwenProtocol.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OwenioNet;
+using OwenioNet.DataConverter.Converter;
 using OwenioNet.IO;
 using OwenioNet.Types;
 
@@ -19,7 +20,7 @@ namespace ASMC.Devices.OWEN
         /// <param name="addresDevice">Удрес устройства.</param>
         /// <param name="ParametrName">Имя параметра, который необходимо считать.</param>
         /// <returns>Массив байт, требующий конверткации.</returns>
-        public byte[] OwenReadParam(int PortNumber, int addresDevice, string ParametrName, ushort? ParIndex = null)
+        public byte[] OwenReadParam(int PortNumber, int addresDevice, string ParametrName, ushort? Register = null)
         {
             SerialPortAdapter port = new SerialPortAdapter(PortNumber, 115200, Parity.None, 8, StopBits.One);
 
@@ -48,7 +49,7 @@ namespace ASMC.Devices.OWEN
 
             try
             {
-                dataFromDevice = owenProtocol.OwenRead(addresDevice, AddressLengthType.Bits8, ParametrName, ParIndex);
+                dataFromDevice = owenProtocol.OwenRead(addresDevice, AddressLengthType.Bits8, ParametrName, Register);
             }
             catch (Exception)
             {
@@ -69,8 +70,8 @@ namespace ASMC.Devices.OWEN
         /// <param name="addressLengthType">Длина сетевого адреса устройства.</param>
         /// <param name="ParametrName">Наименование параметра устройства.</param>
         /// <param name="writeDataBytes">Массив байт дял записи в устройство (значение параметра).</param>
-        /// <param name="ParIndex">Индекс параметра (если есть).</param>
-        public void OwenWriteParam(int PortNumber, int addresDevice, AddressLengthType addressLengthType, string ParametrName, byte[] writeDataBytes, ushort? ParIndex = null)
+        /// <param name="Register">Индекс параметра (если есть).</param>
+        public void OwenWriteParam(int PortNumber, int addresDevice, AddressLengthType addressLengthType, string ParametrName, byte[] writeDataBytes, ushort? Register = null)
         {
             SerialPortAdapter port = new SerialPortAdapter(PortNumber, 115200, Parity.None, 8, StopBits.One);
 
@@ -100,7 +101,7 @@ namespace ASMC.Devices.OWEN
             try
             {
                 //dataFromDevice = owenProtocol.OwenRead(addresDevice, AddressLengthType.Bits8, ParametrName, ParIndex);
-                owenProtocol.OwenWrite(addresDevice, AddressLengthType.Bits8, ParametrName, writeDataBytes, ParIndex);
+                owenProtocol.OwenWrite(addresDevice, AddressLengthType.Bits8, ParametrName, writeDataBytes, Register);
             }
             catch (Exception)
             {
@@ -110,6 +111,62 @@ namespace ASMC.Devices.OWEN
 
             port.Close();
 
+
+        }
+
+        /// <summary>
+        /// Функция для считывания параметра типа string.
+        /// </summary>
+        /// <param name="PortNumber">Номер последовательного порта.</param>
+        /// <param name="addresDevice">Адрес устройства.</param>
+        /// <param name="addressLengthType">Длина сетевого адреса устройства.</param>
+        /// <param name="ParametrName">Наименование параметра устройства.</param>
+        /// <param name="Register">Индекс параметра (если есть).</param>
+        /// <returns>Считанное значение параметра.</returns>
+        public string ReadStringAscii(int PortNumber, int addresDevice, AddressLengthType addressLengthType, string ParametrName, ushort? Register = null)
+        {
+            byte[] answerDevice = OwenReadParam(PortNumber, addresDevice, ParametrName,  Register);
+            var converter = new ConverterAscii(answerDevice.Length);
+            string value = converter.ConvertBack(answerDevice);
+            return value;
+
+        }
+
+        /// <summary>
+        /// Функция для считывания целочисленного параметра (unsigned short int).
+        /// </summary>
+        /// <param name="PortNumber">Номер последовательного порта.</param>
+        /// <param name="addresDevice">Адрес устройства.</param>
+        /// <param name="addressLengthType">Длина сетевого адреса устройства.</param>
+        /// <param name="ParametrName">Наименование параметра устройства.</param>
+        /// <param name="size">Размер ожидаемого числа в байтах.</param>
+        /// <param name="Register">Индекс параметра (если есть).</param>
+        /// <returns>Считанное значение параметра.</returns>
+        public int ReadShortIntParam(int PortNumber, int addresDevice, AddressLengthType addressLengthType, string ParametrName, int size, ushort? Register = null)
+        {
+            byte[] answerDevice = OwenReadParam(PortNumber, addresDevice, ParametrName, Register);
+            var converter = new ConverterI(size);
+            int value = converter.ConvertBack(answerDevice);
+            return value;
+
+        }
+
+        /// <summary>
+        /// Функция для считывания параметра типа float.
+        /// </summary>
+        /// <param name="PortNumber">Номер последовательного порта.</param>
+        /// <param name="addresDevice">Адрес устройства.</param>
+        /// <param name="addressLengthType">Длина сетевого адреса устройства.</param>
+        /// <param name="ParametrName">Наименование параметра устройства.</param>
+        /// <param name="size">Размер ожидаемого числа в байтах.</param>
+        /// <param name="Register">Индекс параметра (если есть).</param>
+        /// <returns>Считанное значение параметра.</returns>
+        public float ReadFloatParam(int PortNumber, int addresDevice, AddressLengthType addressLengthType, string ParametrName, int size, ushort? Register = null)
+        {
+            byte[] answerDevice = OwenReadParam(PortNumber, addresDevice, ParametrName, Register);
+            var converter = new ConverterFloat(size);
+            float value = converter.ConvertBack(answerDevice);
+            return value;
 
         }
     }
