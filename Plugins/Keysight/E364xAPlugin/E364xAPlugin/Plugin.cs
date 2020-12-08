05d4f42792ef016f28b48018ccb141fad2315372
02zaa01 <02zaa01@02-029-003>
2020-12-08 15:48:56 +0300

    Работа идет по плану...

---
+++ Plugins/Keysight/E364xAPlugin/E364xAPlugin/Plugin.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ASMC.Core.Model;
+using ASMC.Data.Model;
+using ASMC.Data.Model.Interface;
+using ASMC.Devices.IEEE;
+using ASMC.Devices.IEEE.Keysight.ElectronicLoad;
+using ASMC.Devices.IEEE.Keysight.Multimeter;
+
+namespace E364xAPlugin
+{
+    public class Plugin<T>  : Program<T> where T : OperationMetrControlBase
+    {
+        public Plugin(ServicePack service) : base(service)
+        {
+            Grsi = "26951-04";
+        }
+    }
+    public class Operation : OperationMetrControlBase
+    {
+        //определяет какие типы проверок доступны для СИ: поверка первичная/переодическая, калибровка, adjustment.
+        public Operation(ServicePack servicePac)
+        {
+            //это операция первичной поверки
+            UserItemOperationPrimaryVerf = new OpertionFirsVerf(servicePack);
+            //здесь периодическая поверка, но набор операций такой же
+            UserItemOperationPeriodicVerf = UserItemOperationPrimaryVerf;
+        }
+    }
+
+    public class OpertionFirsVerf : ASMC.Core.Model.Operation
+    {
+        public OpertionFirsVerf(ServicePack servicePack) : base(servicePack)
+        {
+            ControlDevices = new IDeviceUi[]
+            {
+                new Device
+                {
+                    Devices =   new IDeviceBase[]{new N3303A(), new N3306A()}, Description = "Электронная нагрузка"
+                },
+                new Device
+                {
+                    Devices = new IDeviceBase[] {new Mult_34401A() }, Description = "Цифровой мультиметр"
+                }, 
+            };
+            DocumentName = "E364xA";
+        }
+
+        public override void RefreshDevice()
+        {
+            AddresDevice = IeeeBase.AllStringConnect;
+        }
+
+        public override void FindDevice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Oper1VisualTest : ParagraphBase<bool>
+    {
+        public Oper1VisualTest(IUserItemOperation userItemOperation) : base(userItemOperation)
+        {
+            Name = "Внешний осмотр";
+            DataRow = new List<IBasicOperation<bool>>();
+        }
+
+        #region Methods
+
+        /// <inheritdoc />
+        protected override DataTable FillData()
+        {
+            var data = base.FillData();
+            var dataRow = data.NewRow();
+            if (DataRow.Count == 1)
+            {
+                var dds = DataRow[0] as BasicOperation<bool>;
+                // ReSharper disable once PossibleNullReferenceException
+                dataRow[0] = dds.Getting ? "Соответствует" : dds.Comment;
+                data.Rows.Add(dataRow);
+            }
+
+            return data;
+        }
+
+        /// <inheritdoc />
+        protected override string[] GenerateDataColumnTypeObject()
+        {
+            return new[] { "Результат внешнего осмотра" };
+        }
+
+        /// <inheritdoc />
+        protected override string GetReportTableName()
+        {
+            return "ITBmVisualTest";
+        }
+
+        protected override void InitWork(CancellationTokenSource token)
+        {
+            base.InitWork(token);
+            var operation = new BasicOperation<bool>();
+            operation.Expected = true;
+            operation.IsGood = () => Equals(operation.Getting, operation.Expected);
+            operation.InitWork = () =>
+            {
+                var service = UserItemOperation.ServicePack.QuestionText();
+                service.Title = "Внешний осмотр";
+                service.Entity = new Tuple<string, Assembly>("VisualTest", null);
+                service.Show();
+                var res = service.Entity as Tuple<string, bool>;
+                operation.Getting = res.Item2;
+                operation.Comment = res.Item1;
+                operation.IsGood = () => operation.Getting;
+
+                return Task.CompletedTask;
+            };
+
+            operation.CompliteWork = () => { return Task.FromResult(true); };
+            DataRow.Add(operation);
+        }
+
+        #endregion
+    }
+}
