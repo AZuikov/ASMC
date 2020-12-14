edc5d758a116cad3ba6292d804534e99beeb6d63
02zaa01 <02zaa01@02-029-003>
2020-12-14 11:34:59 +0300

    Делаем абстрактный класс для осциллографов TRM Rohde&Schwarz и думаем над интерфейчом для осциллографов

---
+++ ASMC.Devices/Rohde_Schwarz/IOscilloscope.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMC.Devices.Rohde_Schwarz
+{
+    interface IOscilloscope
+    {
+        /// <summary>
+        /// Выбирает канал, с которым дальше работаем
+        /// </summary>
+        void SetWorkingChanel();
+    }
+}
