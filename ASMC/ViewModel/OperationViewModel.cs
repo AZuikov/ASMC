274ea3eba5f56ba84115b6d5ea0e2fdbade95fc2
02tav01 <02tav01@zrt.aa.local>
2021-01-29 15:32:30 +0300

    Начат рефакторинг операций

---
+++ ASMC/ViewModel/OperationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASMC.Core.ViewModel;
+using DevExpress.Mvvm;
+
+namespace ASMC.ViewModel
+{
+    public class OperationViewModel : ViewModelBase
+    {
+    }
+}
