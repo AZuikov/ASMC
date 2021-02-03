909889841cc598a83c6082d329513ecaf88b8694
02zaa01 <02zaa01@02-029-003>
2021-02-03 15:07:15 +0300

    Делаем интерфейс для источников и измерителей физических величин

--- ASMC.Devices/Interface/ICalibrator.cs
+++ ASMC.Devices/Interface/ICalibrator.cs
@@ -1,6 +1,7 @@
 ﻿using ASMC.Data.Model;
 using ASMC.Data.Model.PhysicalQuantity;
 using ASMC.Devices.IEEE.Fluke.Calibrator;
+using NLog.LayoutRenderers.Wrappers;
 
 namespace ASMC.Devices.Interface
 {
@@ -26,5 +27,82 @@ namespace ASMC.Devices.Interface
 
     void Reset();
     bool IsEnableOutput { get; set; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IRangePhysicalQuantity<TPhysicalQuantity> where TPhysicalQuantity : class, IPhysicalQuantity<TPhysicalQuantity>, new()
+    {
+         RangeStorage<PhysicalRange<TPhysicalQuantity>> Ranges { get; }
+         /// <summary>
+         /// Выбранный предел измерения.
+         /// </summary>
+         PhysicalRange<TPhysicalQuantity> SelectRange { get; }
+
+         /// <summary>
+         /// Установить диапазон физ. величины.
+         /// </summary>
+         /// <param name="inRange">Предел, который нужно установить.</param>
+         void SetRange(PhysicalRange<TPhysicalQuantity> inRange);
+        /// <summary>
+        /// Установить диапазон физ. величины.
+        /// </summary>
+        /// <param name="inRange">Величина, для которой нужно подобрать и установить диапазон.</param>
+        void SetRange(MeasPoint<TPhysicalQuantity> inRange);
+
+         /// <summary>
+         /// Автоматический выбор предела.
+         /// </summary>
+         bool AutoRange { get; set; }
+
+
+
+    }
+
+    public interface ISourcePhysicalQuantityBase: IDevice
+    {
+        /// <summary>
+        /// Установить значение величины.
+        /// </summary>
+        void SetValue();
+
+        bool IsEnableOutput { get; }
+        void OutputOn();
+        void OutputOff();
+
+        
+    }
+
+    /// <summary>
+    /// Интерфейс источника одной физической величины.
+    /// </summary>
+    /// <typeparam name="TPhysicalQuantity">Физическая величина.</typeparam>
+    public interface ISourcePhysicalQuantity<TPhysicalQuantity> : ISourcePhysicalQuantityBase where TPhysicalQuantity : class, IPhysicalQuantity<TPhysicalQuantity>, new()  
+    {
+        RangeStorage<PhysicalRange<TPhysicalQuantity>> RangeStorage { get; set; }
+
+       
+
+    }
+
+    /// <summary>
+    /// Интерфейс источника составной физической величины.
+    /// </summary>
+    /// <typeparam name="TPhysicalQuantity">Физическая величина.</typeparam>
+    /// /// <typeparam name="TPhysicalQuantity2">Физическая величина.</typeparam>
+    public interface ISourceOfPhysicalQuantityBase<TPhysicalQuantity, TPhysicalQuantity2> : ISourcePhysicalQuantityBase where TPhysicalQuantity : class, IPhysicalQuantity<TPhysicalQuantity>, new() where TPhysicalQuantity2 : class, IPhysicalQuantity<TPhysicalQuantity2>, new() 
+    {
+        RangeStorage<PhysicalRange<TPhysicalQuantity, TPhysicalQuantity2>> RangeStorage { get; set; }
+    }
+
+    /// <summary>
+    /// Измерение физической величины.
+    /// </summary>
+    /// <typeparam name="TPhysicalQuantity"></typeparam>
+    public interface IMeterPhysicalQuantity<TPhysicalQuantity>: IDevice where TPhysicalQuantity : class, IPhysicalQuantity<TPhysicalQuantity>, new()
+    {
+        RangeStorage<PhysicalRange<TPhysicalQuantity>> RangeStorage { get; set; }
+        public MeasPoint<TPhysicalQuantity> GetValue();
     }
 }
