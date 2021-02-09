1cb34f27d6b5be13b4c10e02eca0b4476edc2b6b
02zaa01 <02zaa01@02-029-003>
2021-02-09 09:58:56 +0300

    начинаю делать драйвер для В7-40/1

---
+++ ASMC.Devices/IEEE/Belvar_V7_40_1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMC.Devices.IEEE
+{
+    class Belvar_V7_40_1
+    {
+
+    }
+}
---
+++ Plugins/Belvar_V7_40_1/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASMC.Core.Model;
+
+namespace Belvar_V7_40_1
+{
+    public class Main : Program<S801>
+    {
+        /// <inheritdoc />
+        public Main(ServicePack service) : base(service)
+        {
+            Type = Название;
+            Grsi = Горсреестр или методика;
+            Range = Диапазон;
+        }
+    }
+    public class S801 : OperationMetrControlBase
+    {
+
+        public S801(ServicePack servicePac)
+        {
+            UserItemOperationPrimaryVerf = new OperationPrimary<Тип устройства>(Название методики МК, servicePac);
+        }
+
+    }
+
+
+}
