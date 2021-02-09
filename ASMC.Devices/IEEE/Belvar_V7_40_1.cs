0542538bf36d355ed55380a79df08d72b9a66720
02zaa01 <02zaa01@02-029-003>
2021-02-09 10:37:06 +0300

    Работа идет по плану...

--- ASMC.Devices/IEEE/Belvar_V7_40_1.cs
+++ ASMC.Devices/IEEE/Belvar_V7_40_1.cs
@@ -3,11 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ASMC.Data.Model;
+using ASMC.Data.Model.PhysicalQuantity;
+using ASMC.Devices.Interface.Multimetr.Mode;
+using ASMC.Devices.Interface.SourceAndMeter;
 
 namespace ASMC.Devices.IEEE
 {
-    class Belvar_V7_40_1
+   public class Belvar_V7_40_1 :  IAcVoltage, IDcVoltage, IDcCurrent, IAcCurrent, IResistance2W, IProtocolStringLine
     {
+        public IMeterPhysicalQuantity<Current> AcVoltage { get; }
+        public IMeterPhysicalQuantity<Voltage> DcVoltage { get; }
+        public IMeterPhysicalQuantity<Current> DcCurrent { get; }
+        public IMeterPhysicalQuantity<Current> AcCurrent { get; }
+        public IMeterPhysicalQuantity<Resistance> Resistance2W { get; }
+        public string UserType { get; }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
 
+        public bool IsTestConnect { get; }
+        public async Task InitializeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string StringConnection { get; set; }
     }
+
+   public abstract class MeasureFunctionV_7_40_1Base<T> : IMeterPhysicalQuantity<T>
+       where T : class, IPhysicalQuantity<T>, new()
+   {
+       protected IeeeBase _device;
+       protected string ActivateThisModeCommand;
+       protected string FunctionName;
+       protected string RangeCommand;
+
+        public void Getting()
+       {
+           throw new NotImplementedException();
+       }
+
+       public void Setting()
+       {
+           throw new NotImplementedException();
+       }
+
+       public IRangePhysicalQuantity<T> RangeStorage { get; }
+       public MeasPoint<T> GetValue()
+       {
+           string readStr = _device.ReadRawString(12); //одна посылка 12 байт
+       }
+
+       public MeasPoint<T> Value { get; }
+   }
 }
